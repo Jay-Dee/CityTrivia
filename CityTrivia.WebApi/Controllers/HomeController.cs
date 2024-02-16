1d8ddf98459b726508a3dec4c79561924c2cc02a
Jay-Dee <johnsondesouza@gmail.com>
2024-02-16 17:24:54 +0000

    Added home route implementation

---
+++ CityTrivia.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityTrivia.WebApi.Controllers {
+    [Route("/")]
+    public class HomeController : ControllerBase {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult Get() {
+            return Ok(DateTime.UtcNow.ToString());
+        }
+    }
+}
